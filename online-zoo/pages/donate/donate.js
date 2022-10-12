78200cf89fe0a3f9b77d8c7519ad0ff4d9de572e
Denis Miranovich <denisbvb0@gmail.com>
2022-10-12 14:39:08 +0300

    feat: input type range

--- online-zoo/pages/donate/donate.js
+++ online-zoo/pages/donate/donate.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('.donate-amount-input-input');
-const amountNums = document.querySelectorAll('.pick-feed-values-item');
-const amountLine = document.querySelectorAll('.pick-feed-slider-dot');
+const input = document.querySelector('.donate__amount_input-input');
+const amountNums = document.querySelectorAll('.donate__amount_num-item');
+const amountLine = document.querySelectorAll('.donate__amount_line-item');
 
 amountLine.forEach(item => {
     item.checked = false;
@@ -12,9 +12,9 @@ function setValueOnAmountLine(event) {
     if (input.value > 9999)
         input.value = input.value.slice(0, 4);
     amountNums.forEach(num => {
-        num.classList.remove('.pick-feed-values-item-active');
+        num.classList.remove('donate__amount_num-item-active');
         if (event.target.value === num.textContent.slice(1))
-            num.classList.add('.pick-feed-values-item-active');
+            num.classList.add('donate__amount_num-item-active');
         amountLine.forEach(item => {
             item.checked = false;
             if (event.target.value === item.attributes.id.value)
@@ -26,9 +26,9 @@ function setValueOnAmountLine(event) {
 function setValueInInput(event) {
     event.target.checked = true;
     amountNums.forEach(num => {
-        num.classList.remove('.pick-feed-values-item-active');
+        num.classList.remove('donate__amount_num-item-active');
         if (event.target.attributes.id.value === num.textContent.slice(1))
-            num.classList.add('.pick-feed-values-item-active');
+            num.classList.add('donate__amount_num-item-active');
     });
     input.value = event.target.attributes.id.value;
 }
